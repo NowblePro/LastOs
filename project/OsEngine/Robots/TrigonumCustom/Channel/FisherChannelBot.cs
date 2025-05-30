c0297f4c495c613079f013f5ab9c7776811d2c44
idrisov <idrisov@cgeng.pro>
2025-05-30 15:21:40 +0300

    atr stop added to fisher

--- project/OsEngine/Robots/TrigonumCustom/Channel/FisherChannelBot.cs
+++ project/OsEngine/Robots/TrigonumCustom/Channel/FisherChannelBot.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using OsEngine.Charts.CandleChart.Indicators;
 using OsEngine.Entity;
 using OsEngine.Indicators;
+using OsEngine.Market.Servers.Bitfinex.BitfitnexEntity;
 using OsEngine.Market.Servers.YahooFinance.Entity;
 using OsEngine.OsTrader.Panels;
 using OsEngine.OsTrader.Panels.Attributes;
@@ -53,6 +54,21 @@ namespace OsEngine.Robots.TrigonumCustom.Channel
         private StrategyParameterString PointOrPercent;
         #endregion
 
+        #region ATR
+        private StrategyParameterInt LengthAtr;
+        private StrategyParameterDecimal MultiplierAtr;
+        private StrategyParameterBool AtrFilterIsOn;
+
+        Aindicator _ATR;
+
+        private decimal _lastAtr;
+        private decimal _averageAtr;
+        private decimal _lastCandleClose;
+        private bool _needUpdateLastIndex;
+        private bool _needUpdateIterator;
+        private int _iterator = 1;
+        #endregion
+
         private BotTabSimple tab;
         private FisherTransformIndicator fisher;
         private Aindicator ps;
@@ -93,6 +109,14 @@ namespace OsEngine.Robots.TrigonumCustom.Channel
             ChangeStepStop = CreateParameter("Stop level change step", 100, 1, 10000, 001m, "Trailing Stop");
             MinDist = CreateParameter("Minimum distance to price", 500, 1, 10000, 0.01m, "Trailing Stop");
             QuantityStepsPrices = CreateParameter("Quantity steps prices for limit order", 1000m, 0, 10000, 1, "Trailing Stop");
+            #endregion
+
+            #region ATR
+            LengthAtr = CreateParameter("Length ATR", 96, 7, 1000, 1, "ATR");
+            MultiplierAtr = CreateParameter("Multiplier Atr", 1, 1m, 10, 1, "ATR");
+            AtrFilterIsOn = CreateParameter("Is Atr Filter On", false, "ATR");
+            _ATR = IndicatorsFactory.CreateIndicatorByName("ATR", name + "Atr", false);
+            _ATR = (Aindicator)tab.CreateCandleIndicator(_ATR, "NewArea");
             #endregion
 
             ps = IndicatorsFactory.CreateIndicatorByName(nameClass: "ParabolicSAR", name: name + "Parabolic", canDelete: false);
@@ -136,10 +160,18 @@ namespace OsEngine.Robots.TrigonumCustom.Channel
             fisher.SmaPeriod = smaPeriod.ValueInt;
         }
 
+        private void SetATRParameters()
+        {
+            ((IndicatorParameterInt)_ATR.Parameters[0]).ValueInt = LengthAtr.ValueInt;
+            _ATR.Save();
+            _ATR.Reload();
+        }
+
         private void UpdateParameters()
         {
             SetFisherParameters();
             SetTrailingParameters();
+            SetATRParameters();
         }
 
         private void Tab_PositionOpeningSuccesEvent(Position position)
@@ -325,6 +357,14 @@ namespace OsEngine.Robots.TrigonumCustom.Channel
                 return;
             }
 
+            if (AtrFilterIsOn.ValueBool)
+            {
+                if (AtrLogic(candles, candles.Last().Close))
+                {
+                    return;
+                }
+            }
+
             for (int i = 0; i < positions.Count; i++)
             {
                 tab.SellAtStopCancel();
@@ -378,6 +418,61 @@ namespace OsEngine.Robots.TrigonumCustom.Channel
                 volume = Math.Round(volume, tab.Security.DecimalsVolume);
             }
             return volume;
+        }
+
+        private bool AtrLogic(List<Candle> candles, decimal lastCandle)
+        {
+            if (_ATR.DataSeries[0].Last == 0 && _needUpdateIterator)
+            {
+                _lastCandleClose = 0;
+                _averageAtr = 0;
+                _iterator = 1;
+                _needUpdateIterator = false;
+            }
+
+            if (candles.Count < LengthAtr.ValueInt)
+            {
+                return true;
+            }
+
+            _lastAtr = _ATR.DataSeries[0].Last;
+
+            if (_ATR.DataSeries[0].Values.Count >= LengthAtr.ValueInt * _iterator)
+            {
+                _lastCandleClose = lastCandle;
+                _averageAtr = _lastAtr;
+                _iterator++;
+                _needUpdateLastIndex = false;
+                _needUpdateIterator = true;
+            }
+
+            if (_needUpdateLastIndex || Math.Abs(lastCandle - _lastCandleClose) > _averageAtr * MultiplierAtr.ValueDecimal)
+            {
+                if (tab.PositionsOpenAll.Count > 0)
+                {
+                    CancelStopsAndProfits();
+                }
+                _needUpdateLastIndex = true;
+                return true;
+            }
+
+            return false;
+        }
+
+        private void CancelStopsAndProfits()
+        {
+            List<Position> positions = tab.PositionsOpenAll;
+
+            for (int i = 0; i < positions.Count; i++)
+            {
+                Position pos = positions[i];
+
+                pos.StopOrderIsActiv = false;
+                pos.ProfitOrderIsActiv = false;
+            }
+
+            tab.BuyAtStopCancel();
+            tab.SellAtStopCancel();
         }
 
         public override string GetNameStrategyType() => $"{nameof(FisherChannelBot)}";
