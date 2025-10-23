dd10d3b94a2200f26945ec875f7c85af02cb68ca
idrisov <idrisov@cgeng.pro>
2025-10-23 17:18:47 +0300

    robot mean reverse 2: grid painting, logic

--- project/OsEngine/Indicators/TrigonumCustom/ZScore.cs
+++ project/OsEngine/Indicators/TrigonumCustom/ZScore.cs
@@ -14,13 +14,14 @@ namespace OsEngine.Indicators.TrigonumCustom
     {
         private Aindicator _sma;
         private IndicatorDataSeries _seriesZ;
+        private IndicatorDataSeries _seriesSigma;
         /// <summary>
         /// Ширина окна для расчёта отклонения
         /// </summary>
         private IndicatorParameterInt _window_sigma;
         public override void OnProcess(List<Candle> source, int index)
         {
-            if (_window_sigma.ValueInt > index)
+            if (_window_sigma.ValueInt > index || _sma == null)
             {
                 return;
             }
@@ -29,11 +30,7 @@ namespace OsEngine.Indicators.TrigonumCustom
 
             for (; i < source.Count; i++)
             {
-                if (i < _window_sigma.ValueInt)
-                {
-                    _seriesZ.Values.Add(0);
-                }
-                else
+                if (i >= _window_sigma.ValueInt)
                 {
                     decimal price = source[i].Close;
                     decimal sma = _sma.DataSeries[0].Values[i];
@@ -41,9 +38,20 @@ namespace OsEngine.Indicators.TrigonumCustom
                     decimal sigma = GetSigma(source.Skip(startIndex).Take(_window_sigma.ValueInt).Select(candle => candle.Close));
                     decimal value = (price - sma) / sigma;
                     _seriesZ.Values[i] = value;
+                    _seriesSigma.Values[i] = sigma;
                 }
             }
         }
+
+        public Aindicator SMA
+        {
+            get { return _sma; }
+            set { _sma = value; }
+        }
+
+        public decimal CurrentZ => DataSeries[0].Last;
+
+        public decimal CurrentSigma => DataSeries[1].Last;
 
         private decimal GetSigma(IEnumerable<decimal> source)
         {
@@ -69,14 +77,15 @@ namespace OsEngine.Indicators.TrigonumCustom
                 PaintOn = true;
                 TypeIndicator = IndicatorChartPaintType.Line;
                 NeedToResetDataEvent += Reset;
-                _seriesZ = CreateSeries("_seriesZZHighPoints", Color.GreenYellow, IndicatorChartPaintType.Point, true);
+                _seriesZ = CreateSeries("_seriesZ", Color.GreenYellow, IndicatorChartPaintType.Line, true);
                 _seriesZ.CanReBuildHistoricalValues = false;
                 _seriesZ.ChartPaintType = IndicatorChartPaintType.Line;
 
-                _sma = IndicatorsFactory.CreateIndicatorByName("Sma", "Sma", false);
-                _sma.TypeIndicator = IndicatorChartPaintType.Line;
+                _seriesSigma = CreateSeries("_seriesSigma", Color.Blue, IndicatorChartPaintType.Line, false);
+                _seriesSigma.CanReBuildHistoricalValues = false;
+                _seriesSigma.ChartPaintType = IndicatorChartPaintType.Line;
+
                 _window_sigma = CreateParameterInt("Window Sigma", 30);
-                ProcessIndicator("ZigZag", _sma);
             }
         }
 
--- project/OsEngine/Robots/TrigonumCustom/Base/MeanReversionSma2.cs
+++ project/OsEngine/Robots/TrigonumCustom/Base/MeanReversionSma2.cs
@@ -1,4 +1,5 @@
 ﻿using OsEngine.Common;
+using OsEngine.Common.UI;
 using OsEngine.Entity;
 using OsEngine.Indicators;
 using OsEngine.Indicators.TrigonumCustom;
@@ -18,21 +19,28 @@ namespace OsEngine.Robots.TrigonumCustom.Base
     {
         private ZScore _zScore;
         private Aindicator _ema;
+        private Aindicator _sma;
 
         private StrategyParameterInt _periodSma;
         private StrategyParameterDecimal _zEnterBaseLong;
         private StrategyParameterDecimal _zEnterBaseShort;
         private StrategyParameterInt _periodEma;
+        private StrategyParameterDecimal _spread;
+        private MeanReverseGrid _currentGrid = null;
 
         public MeanReversionSma2(string name, StartProgram startProgram) : base(name, startProgram)
         {
             _multiplePosition = true;
             _periodSma = CreateParameter("SMA period", 20, 20, 400, 1, "Robot parameters");
-            _periodEma = CreateParameter("EMA period", 200, 14, 250, 1, "Robot parameters");
+            _periodEma = CreateParameter("EMA period", 200, 100, 300, 1, "Robot parameters");
+
+            _sma = (Aindicator)IndicatorsFactory.CreateIndicatorByName(nameClass: "Sma", name: name + "Sma", canDelete: false);
+            _sma = (Aindicator)_tab.CreateCandleIndicator(_sma, nameArea: "Prime");
+            _sma.Save();
 
             _zScore = (ZScore)IndicatorsFactory.CreateIndicatorByName(nameClass: "ZScore", name: name + "ZScore", canDelete: false);
             _zScore = (ZScore)_tab.CreateCandleIndicator(_zScore, nameArea: "ZScore");
-            _zScore.ParametersDigit[0].Value = _periodSma.ValueInt;
+            //_zScore.ParametersDigit[0].Value = _periodSma.ValueInt;
             _zScore.DataSeries[0].Color = Color.Red;
             _zScore.Save();
 
@@ -40,13 +48,40 @@ namespace OsEngine.Robots.TrigonumCustom.Base
             _ema = (Aindicator)_tab.CreateCandleIndicator(_ema, nameArea: "Prime");
             _ema.Save();
 
+            _zScore.SMA = _sma;
+
             _zEnterBaseLong = CreateParameter("Z Enter Base Long", -2m, -3m, -1m, 0.1m, "Robot");
             _zEnterBaseShort = CreateParameter("Z Enter Base Short", 2m, 1m, 3m, 0.1m, "Robot");
 
+            _spread = CreateParameter("Spread", 1m, 0.1m, 1m, 0.1m, "Robot");
+
             new TakeProfitDecoration(this);
             new StopLossDecoration(this);
+            _tab.PositionOpeningSuccesEvent += _tab_PositionOpeningSuccesEvent;
+            UpdateParameters();
+        }
 
+        protected override void CandleFinishedEvent(List<Candle> candles)
+        {
+            base.CandleFinishedEvent(candles);
+            if (_tab.PositionsOpenAll.Count == 0)
+            {
+                _currentGrid = null;
+            }
+        }
+
+        private void _tab_PositionOpeningSuccesEvent(Position obj)
+        {
+            if (_currentGrid == null)
+            {
+                _currentGrid = new MeanReverseGrid(obj.EntryPrice, _spread.ValueDecimal, _zScore.CurrentSigma, 7, obj.Direction, _tab.GetChartMaster().Candles.Count - 1);
+            }
+        }
+
+        private void UpdateParameters()
+        {
             SetEmaPeriod();
+            SetSmaPeriod();
         }
 
         private void SetEmaPeriod()
@@ -57,6 +92,14 @@ namespace OsEngine.Robots.TrigonumCustom.Base
             }
         }
 
+        private void SetSmaPeriod()
+        {
+            if (_sma?.Parameters[0] is IndicatorParameterInt parameter)
+            {
+                parameter.ValueInt = _periodSma.ValueInt;
+            }
+        }
+
         protected override bool CheckClosePosition(List<Candle> candles, Position position)
         {
             return false;
@@ -64,25 +107,74 @@ namespace OsEngine.Robots.TrigonumCustom.Base
 
         protected override bool CheckOpenLongPosition(List<Candle> candles)
         {
-            decimal z = _zScore.DataSeries[0].Last;
-            decimal ema = _ema.DataSeries[0].Last;
-            decimal price = candles.Last().Close;
-            if (z < _zEnterBaseLong.ValueDecimal && ema < price)
+            if (_currentGrid != null && _currentGrid.Direction == Side.Sell) return false;
+
+            if (_currentGrid != null)
             {
-                return true;
+                decimal price = candles.Last().Low;
+                decimal ema = _ema.DataSeries[0].Last;
+                Dictionary<int, bool> dict = _currentGrid.GetDict();
+                Dictionary<int, decimal> grid = _currentGrid.GetGrid();
+                IEnumerable<KeyValuePair<int, decimal>> pairs = grid.Where(pair => pair.Value >= price && pair.Value > ema);
+                bool result = false;
+                foreach (KeyValuePair<int, decimal> pair in pairs)
+                {
+                    if (!dict[pair.Key])
+                    {
+                        dict[pair.Key] = true;
+                        result |= true;
+                    }
+                }
+                return result;
             }
+            else
+            {
+                decimal z = _zScore.CurrentZ;
+                decimal ema = _ema.DataSeries[0].Last;
+                decimal price = candles.Last().Close;
+                if (z < _zEnterBaseLong.ValueDecimal && price > ema)
+                {
+                    return true;
+                }
+            }
+
             return false;
         }
 
         protected override bool CheckOpenShortPosition(List<Candle> candles)
         {
-            decimal z = _zScore.DataSeries[0].Last;
-            decimal ema = _ema.DataSeries[0].Last;
-            decimal price = candles.Last().Close;
-            if (z > _zEnterBaseShort.ValueDecimal && ema > price)
+            if (_currentGrid != null && _currentGrid.Direction == Side.Buy) return false;
+
+
+            if (_currentGrid != null)
             {
-                return true;
+                decimal price = candles.Last().High;
+                decimal ema = _ema.DataSeries[0].Last;
+                Dictionary<int, bool> dict = _currentGrid.GetDict();
+                Dictionary<int, decimal> grid = _currentGrid.GetGrid();
+                IEnumerable<KeyValuePair<int, decimal>> pairs = grid.Where(pair => pair.Value <= price && pair.Value < ema);
+                bool result = false;
+                foreach (KeyValuePair<int, decimal> pair in pairs)
+                {
+                    if (!dict[pair.Key])
+                    {
+                        dict[pair.Key] = true;
+                        result |= true;
+                    }
+                }
+                return result;
             }
+            else
+            {
+                decimal z = _zScore.CurrentZ;
+                decimal ema = _ema.DataSeries[0].Last;
+                decimal price = candles.Last().Close;
+                if (z > _zEnterBaseShort.ValueDecimal && price < ema)
+                {
+                    return true;
+                }
+            }
+
             return false;
         }
 
@@ -90,18 +182,118 @@ namespace OsEngine.Robots.TrigonumCustom.Base
         {
             return new List<Func<List<Candle>, bool>>() 
             {
-                (candles) => { return candles.Count >= _periodSma.ValueInt; }
+                (candles) => { return candles.Count >= _periodSma.ValueInt && candles.Count >= _periodEma.ValueInt; }
             };
         }
 
         protected override void OnChartPostPaint(object sender, ChartPaintEventArgs e)
         {
+            if (_currentGrid == null) return;
+            try
+            {
+                Graphics g = e.ChartGraphics.Graphics;
 
+                Chart chart = e.Chart;
+                ChartArea area = chart?.ChartAreas?.Where(a => a.Name == "Prime").SingleOrDefault();
+                Series series = chart.Series.Where(s => s.ChartType == SeriesChartType.Candlestick).FirstOrDefault();
+
+                if (area != null && series != null)
+                {
+                    Axis xAxis = area.AxisX;
+                    Axis yAxis = area.AxisY;
+
+                    int index = _currentGrid.Index;
+
+                    double minX = xAxis.ScaleView.ViewMinimum;
+                    double maxX = xAxis.ScaleView.ViewMaximum;
+
+                    area.AxisY.Minimum = area.AxisY2.Minimum;
+                    area.AxisY.Maximum = area.AxisY2.Maximum;
+
+                    Dictionary<int, decimal> grid = _currentGrid.GetGrid();
+                    Dictionary<int, bool> dict = _currentGrid.GetDict();
+
+                    using (Brush brush = new SolidBrush(Color.Gray))
+                    using (Pen pen = new Pen(brush))
+                    using (Brush brushGreen = new SolidBrush(Color.Green))
+                    using (Pen penGreen = new Pen(brushGreen))
+                    {
+                        foreach (KeyValuePair<int, decimal> pair in grid)
+                        {
+                            float x1 = (float)xAxis.ValueToPixelPosition(index - 3);
+                            float x2 = (float)xAxis.ValueToPixelPosition(maxX);
+                            float y = (float)yAxis.ValueToPixelPosition((float)pair.Value);
+                            if (dict[pair.Key])
+                            {
+
+                                g.DrawLine(penGreen, x1, y, x2, y);
+                            }
+                            else
+                            {
+                                g.DrawLine(pen, x1, y, x2, y);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
 
         protected override void ParametersChangedByUser()
         {
-            SetEmaPeriod();
+            UpdateParameters();
+        }
+    }
+
+    class MeanReverseGrid
+    {
+        private Side _side;
+        /// <summary>
+        /// Словарь, key - индекс уровня в гриде, value - исполнен ли ордер
+        /// </summary>
+        private Dictionary<int, bool> _dict = new Dictionary<int, bool>();
+        private Dictionary<int, decimal> _grid = new Dictionary<int, decimal>();
+        private int _index;
+
+        public MeanReverseGrid(decimal price, decimal spread, decimal sigma, int levelsCount, Side side, int index)
+        {
+            if (levelsCount < 2) throw new Exception("Уровней должно быть хотя бы 2");
+            _side = side;
+            _index = index;
+            decimal delta = spread * sigma;
+            if (side == Side.Buy)
+            {
+                for (int i = 1; i < levelsCount + 1; i++)
+                {
+                    _grid.Add(i, price - delta * i);
+                    _dict.Add(i, false);
+                }
+            }
+            else
+            {
+                for (int i = 1 ; i < levelsCount + 1; i++)
+                {
+                    _grid.Add(i, price + delta * i);
+                    _dict.Add(i, false);
+                }
+            }
+        }
+
+        public Side Direction => _side;
+
+        public int Index => _index;
+
+        public Dictionary<int, decimal> GetGrid()
+        {
+            return _grid;
+        }
+
+        public Dictionary<int, bool> GetDict()
+        {
+            return _dict;
         }
     }
 }
