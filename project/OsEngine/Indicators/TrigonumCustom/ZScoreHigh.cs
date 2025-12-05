1b66fbbe86b48e16adc0196843a8f8def3bd54e4
idrisov <idrisov@cgeng.pro>
2025-12-05 16:10:43 +0300

    mean reverse z score: z score indicator

---
+++ project/OsEngine/Indicators/TrigonumCustom/ZScoreHigh.cs
@@ -0,0 +1,72 @@
+﻿using OsEngine.Entity;
+using OsEngine.Logging;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsEngine.Indicators.TrigonumCustom
+{
+    [Indicator("ZScoreHigh")]
+    public class ZScoreHigh : Aindicator
+    {
+        private Aindicator _sma;
+        private List<decimal> _deviation = new List<decimal>();
+        private IndicatorDataSeries _seriesZ;
+        /// <summary>
+        /// Ширина окна для расчёта отклонения
+        /// </summary>
+        private IndicatorParameterInt _window_sigma;
+
+        public Aindicator SMA
+        {
+            get { return _sma; }
+            set { _sma = value; }
+        }
+
+        public override void OnProcess(List<Candle> source, int index)
+        {
+            int i = source.Count - 1;
+            Candle candle = source[i];
+            decimal sma = _sma.DataSeries[0].Values[i];
+            if (sma == 0) return;
+            decimal high = Math.Max(0, candle.High - sma);
+            if (high > 0)
+            {
+                _deviation.Add(high);
+            }
+            if (_sma == null || _deviation.Count < _window_sigma.ValueInt)
+            {
+                return;
+            }
+            int skip = _deviation.Count - _window_sigma.ValueInt;
+            decimal avg = _deviation.Skip(skip).Average();
+            decimal sumOfSquares = (decimal)_deviation.Skip(skip).Sum(x => Math.Pow((double)(x - avg), 2));
+            decimal variance = sumOfSquares / _window_sigma.ValueInt - 1;
+            decimal standartDeviation = (decimal)Math.Sqrt((double)variance);
+            decimal result = (high - avg) / standartDeviation;
+            _seriesZ.Values[i] = result;
+        }
+
+        public override void OnStateChange(IndicatorState state)
+        {
+            if (state == IndicatorState.Configure)
+            {
+                PaintOn = true;
+                TypeIndicator = IndicatorChartPaintType.Line;
+                NeedToResetDataEvent += Reset;
+
+                _seriesZ = CreateSeries("_seriesZ", Color.GreenYellow, IndicatorChartPaintType.Line, true);
+                _seriesZ.CanReBuildHistoricalValues = false;
+                _seriesZ.ChartPaintType = IndicatorChartPaintType.Line;
+
+                _window_sigma = CreateParameterInt("Window Sigma", 500);
+            }
+        }
+
+        private void Reset(IIndicator indicator)
+        { }
+    }
+}
---
+++ project/OsEngine/Robots/TrigonumCustom/Base/MeanReversionZScore.cs
@@ -0,0 +1,85 @@
+﻿using OsEngine.Entity;
+using OsEngine.Indicators;
+using OsEngine.Indicators.TrigonumCustom;
+using OsEngine.OsTrader.Panels.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsEngine.Robots.TrigonumCustom.Base
+{
+    [Bot("MeanReversionZScore")]
+    public class MeanReversionZScore : BotPanelSimple
+    {
+        private Aindicator _sma;
+        private ZScoreLow _zScoreLow;
+        private ZScoreHigh _zScoreHigh;
+
+        private StrategyParameterInt _periodSma;
+
+        public MeanReversionZScore(string name, StartProgram startProgram) : base(name, startProgram)
+        {
+            _sma = IndicatorsFactory.CreateIndicatorByName("Sma", name + "Sma", false);
+            _sma = (Aindicator)_tab.CreateCandleIndicator(_sma, "Prime");
+            _periodSma = CreateParameter("Sma Period", 50, 50, 500, 50, "Robot");
+
+            _zScoreLow = (ZScoreLow)IndicatorsFactory.CreateIndicatorByName(nameClass: "ZScoreLow", name: name + "ZScoreLow", canDelete: false);
+            _zScoreLow = (ZScoreLow)_tab.CreateCandleIndicator(_zScoreLow, nameArea: "ZScoreLow");
+            _zScoreLow.DataSeries[0].Color = Color.Blue;
+            _zScoreLow.Save();
+            _zScoreLow.SMA = _sma;
+
+            _zScoreHigh = (ZScoreHigh)IndicatorsFactory.CreateIndicatorByName(nameClass: "ZScoreHigh", name: name + "ZScoreHigh", canDelete: false);
+            _zScoreHigh = (ZScoreHigh)_tab.CreateCandleIndicator(_zScoreHigh, nameArea: "ZScoreHigh");
+            _zScoreHigh.DataSeries[0].Color = Color.Red;
+            _zScoreHigh.Save();
+            _zScoreHigh.SMA = _sma;
+
+            UpdateParameters();
+        }
+
+        protected override bool CheckClosePosition(List<Candle> candles, Position position)
+        {
+            return false;
+        }
+
+        protected override bool CheckOpenLongPosition(List<Candle> candles)
+        {
+            return false;
+        }
+
+        protected override bool CheckOpenShortPosition(List<Candle> candles)
+        {
+            return false;
+        }
+
+        protected override List<Func<List<Candle>, bool>> GetCheckers()
+        {
+            return new List<Func<List<Candle>, bool>>()
+            {
+                candles => true
+            };
+        }
+
+        protected override void ParametersChangedByUser()
+        {
+            UpdateParameters();
+        }
+
+        private void UpdateParameters()
+        {
+            SetSmaPeriod();
+        }
+
+        private void SetSmaPeriod()
+        {
+            if (_sma?.Parameters[0] is IndicatorParameterInt parameter)
+            {
+                parameter.ValueInt = _periodSma.ValueInt;
+            }
+        }
+    }
+}
