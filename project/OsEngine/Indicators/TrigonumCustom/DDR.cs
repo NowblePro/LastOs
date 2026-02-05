bd778bef5b9bcf7ed358789bcddda1e8849d3919
idrisov <idrisov@cgeng.pro>
2026-02-05 12:13:34 +0300

    MRZAtrRrDdr: ddr formula, robot init

--- project/OsEngine/Indicators/TrigonumCustom/DDR.cs
+++ project/OsEngine/Indicators/TrigonumCustom/DDR.cs
@@ -13,15 +13,38 @@ namespace OsEngine.Indicators.TrigonumCustom
     public class DDR : Aindicator
     {
         private IndicatorDataSeries _rt;
-        private IndicatorDataSeries _r;
+        private IndicatorDataSeries _series;
 
         public override void OnProcess(List<Candle> source, int index)
         {
             if (index < 1) return;
+            if (N < 2) return;
             Candle prev = source[index - 1];
             Candle last = source[index];
             decimal Rt = (decimal)Math.Log((double)(last.Close / prev.Close));
             _rt.Values[index] = Rt;
+
+            if (index < N - 1) return;
+            decimal r = 0;
+            for (int i = index - (N - 1); i < index; i++)
+            {
+                r += _rt.Values[i];
+            }
+            r /= N;
+
+            decimal s = 0;
+            for (int i = index - (N - 1); i < index; i++)
+            {
+                s += (decimal)Math.Pow((double)(_rt.Values[i] - r), 2);
+            }
+
+            s /= N - 1;
+            s = (decimal)Math.Sqrt((double)s);
+
+            decimal drift = r * N;
+            decimal diffusion = s * (decimal)Math.Sqrt(N);
+            decimal ddr = Math.Abs(drift) / diffusion;
+            _series.Values[index] = ddr;
         }
 
         /// <summary>
@@ -37,13 +60,13 @@ namespace OsEngine.Indicators.TrigonumCustom
                 TypeIndicator = IndicatorChartPaintType.Line;
                 NeedToResetDataEvent += Reset;
 
-                _rt = CreateSeries("rt", Color.GreenYellow, IndicatorChartPaintType.Line, true);
+                _rt = CreateSeries("rt", Color.GreenYellow, IndicatorChartPaintType.Line, false);
                 _rt.CanReBuildHistoricalValues = false;
                 _rt.ChartPaintType = IndicatorChartPaintType.Line;
 
-                _r = CreateSeries("r", Color.GreenYellow, IndicatorChartPaintType.Line, false);
-                _r.CanReBuildHistoricalValues = false;
-                _r.ChartPaintType = IndicatorChartPaintType.Line;
+                _series = CreateSeries("series", Color.GreenYellow, IndicatorChartPaintType.Line, true);
+                _series.CanReBuildHistoricalValues = false;
+                _series.ChartPaintType = IndicatorChartPaintType.Line;
             }
         }
 
